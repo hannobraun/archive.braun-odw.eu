e6814fb55c953ab855f939dd70fab0630530c3e4
Hanno Braun <hanno@braun-embedded.com>
2021-03-06 17:26:27 +0100

    Refactor

--- rust/dns/src/main.rs
+++ rust/dns/src/main.rs
@@ -3,18 +3,24 @@ use std::{fs::File, io::Read};
 use serde::Deserialize;
 
 fn main() -> anyhow::Result<()> {
-    let mut secrets = Vec::new();
-    File::open("secrets.toml")?.read_to_end(&mut secrets)?;
-    let secrets: Secrets = toml::from_slice(&secrets)?;
-
+    let secrets = Secrets::load()?;
     println!("{:?}", secrets);
-
     Ok(())
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Secrets {
     pub dns: Dns,
+}
+
+impl Secrets {
+    pub fn load() -> anyhow::Result<Self> {
+        let mut secrets = Vec::new();
+        File::open("secrets.toml")?.read_to_end(&mut secrets)?;
+        let secrets = toml::from_slice(&secrets)?;
+
+        Ok(secrets)
+    }
 }
 
 #[derive(Debug, Deserialize)]
