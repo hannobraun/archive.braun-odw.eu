e292985ac6f21c7247799167c0fb9bdb85e8833b
Hanno Braun <hanno@braun-embedded.com>
2021-03-06 17:19:38 +0100

    Load secrets file

--- rust/dns/src/main.rs
+++ rust/dns/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::Read};
+
+fn main() -> anyhow::Result<()> {
+    let mut secrets = Vec::new();
+    File::open("secrets.toml")?.read_to_end(&mut secrets)?;
+
+    println!("{:?}", secrets);
+
+    Ok(())
 }
