3a5af3588aadb25b02758979db81bf42162803bb
Hanno Braun <hanno@braun-embedded.com>
2021-03-06 17:29:57 +0100

    Move `Secrets` to dedicated directory

--- rust/dns/src/main.rs
+++ rust/dns/src/main.rs
@@ -1,30 +1,9 @@
-use std::{fs::File, io::Read};
+mod secrets;
 
-use serde::Deserialize;
+use self::secrets::Secrets;
 
 fn main() -> anyhow::Result<()> {
     let secrets = Secrets::load()?;
     println!("API token: {}", secrets.dns.api_token);
     Ok(())
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Secrets {
-    pub dns: Dns,
-}
-
-impl Secrets {
-    pub fn load() -> anyhow::Result<Self> {
-        let mut secrets = Vec::new();
-        File::open("secrets.toml")?.read_to_end(&mut secrets)?;
-        let secrets = toml::from_slice(&secrets)?;
-
-        Ok(secrets)
-    }
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Dns {
-    #[serde(rename = "api-token")]
-    pub api_token: String,
 }
---
+++ rust/dns/src/secrets.rs
@@ -0,0 +1,24 @@
+use std::{fs::File, io::Read};
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Secrets {
+    pub dns: Dns,
+}
+
+impl Secrets {
+    pub fn load() -> anyhow::Result<Self> {
+        let mut secrets = Vec::new();
+        File::open("secrets.toml")?.read_to_end(&mut secrets)?;
+        let secrets = toml::from_slice(&secrets)?;
+
+        Ok(secrets)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Dns {
+    #[serde(rename = "api-token")]
+    pub api_token: String,
+}
