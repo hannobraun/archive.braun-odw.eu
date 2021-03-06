74f4a89d7afba5a5866e1836ee300c080103b6ff
Hanno Braun <hanno@braun-embedded.com>
2021-03-06 17:23:42 +0100

    Deserialize secrets.toml

--- rust/dns/src/main.rs
+++ rust/dns/src/main.rs
@@ -1,10 +1,24 @@
 use std::{fs::File, io::Read};
 
+use serde::Deserialize;
+
 fn main() -> anyhow::Result<()> {
     let mut secrets = Vec::new();
     File::open("secrets.toml")?.read_to_end(&mut secrets)?;
+    let secrets: Secrets = toml::from_slice(&secrets)?;
 
     println!("{:?}", secrets);
 
     Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Secrets {
+    pub dns: Dns,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Dns {
+    #[serde(rename = "api-token")]
+    pub api_token: String,
 }
