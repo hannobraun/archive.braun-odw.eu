9690c3e862f42bfb2d08a43643e9434c9b311684
Hanno Braun <hanno@braun-embedded.com>
2021-03-08 22:15:59 +0100

    Add dependency on reqwest

--- tools/dns/src/main.rs
+++ tools/dns/src/main.rs
@@ -11,10 +11,6 @@ fn main() -> anyhow::Result<()> {
     println!("API token: {}", secrets.dns.api_token);
     println!("Domain ID: {}", domain.id);
 
-    // TASK: Decide on approach to accessing Hetzner DNS REST API. restson looks
-    //       promising, but I'm getting weird linker errors when compiling it,
-    //       from openssl-sys.
-
     // TASK: Validate zone file.
     // TASK: Upload zone file.
 
