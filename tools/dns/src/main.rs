39a2ed60ccbcbe807aa67871a8534f0160d61857
Hanno Braun <hanno@braun-embedded.com>
2021-03-08 22:22:18 +0100

    Send post request

--- tools/dns/src/main.rs
+++ tools/dns/src/main.rs
@@ -11,6 +11,12 @@ fn main() -> anyhow::Result<()> {
     println!("API token: {}", secrets.dns.api_token);
     println!("Domain ID: {}", domain.id);
 
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post("https://dns.hetzner.com/api/v1/zones/file/validate")
+        .send()?;
+    println!("{} {}", response.status(), response.text()?);
+
     // TASK: Validate zone file.
     // TASK: Upload zone file.
 
