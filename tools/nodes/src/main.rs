eed74b758b11b413e2a14bab42f5d2995f1740fa
Hanno Braun <hanno@braun-embedded.com>
2021-03-11 21:25:40 +0100

    Run docker image after building it

--- tools/nodes/src/main.rs
+++ tools/nodes/src/main.rs
@@ -1,15 +1,20 @@
 use std::process::Command;
 
 fn main() -> anyhow::Result<()> {
+    let name = "nodes-test";
+
     let status = Command::new("docker")
         .arg("build")
         .arg("-t")
-        .arg("nodes-test")
+        .arg(name)
         .arg("-f")
         .arg("tools/nodes/docker/Dockerfile")
         .arg("nodes/")
         .status()?;
 
+    println!("\n{}\n\n", status);
+
+    let status = Command::new("docker").arg("run").arg(name).status()?;
     println!("\n{}", status);
 
     Ok(())
