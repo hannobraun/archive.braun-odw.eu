5022074b72fe40676bd158a4f0fb9f5a9e0f5049
Hanno Braun <hanno@braun-embedded.com>
2021-03-11 21:07:28 +0100

    Directly show output from Docker

--- tools/nodes/src/main.rs
+++ tools/nodes/src/main.rs
@@ -1,14 +1,14 @@
 use std::process::Command;
 
 fn main() -> anyhow::Result<()> {
-    let output = Command::new("docker")
+    let status = Command::new("docker")
         .arg("build")
         .arg("-f")
         .arg("tools/nodes/docker/Dockerfile")
         .arg("nodes/")
-        .output()?;
+        .status()?;
 
-    println!("{:?}", output);
+    println!("{:?}", status);
 
     Ok(())
 }
