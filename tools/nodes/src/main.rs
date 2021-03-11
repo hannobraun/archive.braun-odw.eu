1284be458f6604b1b852592181e5589c4ac5eccf
Hanno Braun <hanno@braun-embedded.com>
2021-03-11 21:24:26 +0100

    Assign name to docker image

--- tools/nodes/src/main.rs
+++ tools/nodes/src/main.rs
@@ -3,6 +3,8 @@ use std::process::Command;
 fn main() -> anyhow::Result<()> {
     let status = Command::new("docker")
         .arg("build")
+        .arg("-t")
+        .arg("nodes-test")
         .arg("-f")
         .arg("tools/nodes/docker/Dockerfile")
         .arg("nodes/")
