31beab25777c2a05fa41a63e7d435727d1a4a398
Hanno Braun <hanno@braun-embedded.com>
2021-03-11 21:03:37 +0100

    Build Dockerfile

--- tools/nodes/src/main.rs
+++ tools/nodes/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+fn main() -> anyhow::Result<()> {
+    let output = Command::new("docker")
+        .arg("build")
+        .arg("-f")
+        .arg("tools/nodes/docker/Dockerfile")
+        .arg("nodes/")
+        .output()?;
+
+    println!("{:?}", output);
+
+    Ok(())
 }
