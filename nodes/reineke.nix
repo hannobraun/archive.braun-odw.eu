a5494b203df56b5c929c5e854a710bc2cf90ed65
Hanno Braun <hanno@braun-embedded.com>
2021-03-13 14:00:25 +0100

    Add placeholder for deployment file

---
+++ nodes/reineke.nix
@@ -0,0 +1,6 @@
+# TASK: Import basic configuration from server.
+# TASK: Deploy an SSH key, set up login into root using that key:
+#       - https://nixos.org/manual/nixos/stable/index.html#sec-ssh
+#       - Search for "deployment.keys" in NixOps manual.
+# TASK: Set `users.mutableUsers` to `false`:
+#       https://nixos.org/manual/nixos/stable/index.html#sec-user-management
