039ad725e5d60229cf4e468bd3eed91d14696868
Hanno Braun <hanno@braun-embedded.com>
2021-03-16 20:45:54 +0100

    Deploy SSH key on server

--- nodes/reineke.nix
+++ nodes/reineke.nix
@@ -4,7 +4,7 @@
     {
       deployment = {
         targetHost = "reineke.nodes.braun-odw.eu";
-        # TASK: Set up private SSH key.
+        keys.root.text = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpUT/y8rl08ZxWUVIAj2LHh1dapmG8S6DqZLDaqKlBEhEj3w/DC3991+NA/3I8O9ITvwGeox3EC/WMNb0NYq1jgLACvIc+ig14b69U86HbVMcTJqyCkc0Bf/zgbEnH+HxzKsPGFBLjlISHIInwwquoxDCa3sR8LVuhCUc2YYiRcgIbXxUcwxlMrSrJuKmsfMDBdGACTK4AvgR7q7SXVjypCvU+joPmX9d8IKZRg59AQkWnZAdulNPF/xk53wSZlkNynh6JhWjU28x/1XUSkK+JHVKUoaQgRFmf9OdqmT7YCi9KfP6/ipAJcB41N1/zDwahIy6sGxtx+TjEPGKsGu2RJMKdjwXioMcQNgoHhuQhJZTiimnJJz5Y6DzUdgNsZkRHFmoinbZ71TFGGppLijMC173sOioMSToNuyHEJKu91bDDxJfaZE9DQCh4nGxEJYNxUwlO2YIMzFVHWyYQ5IpSi8CfWmnWIJTLltVQOzPnFMt5N1ZCIN/O0NYMHzPmjE8= root@reineke.nodes.braun-odw.eu";
       };
 
       imports = [
@@ -14,9 +14,6 @@
 }
 
 # TASK: Import basic configuration from server.
-# TASK: Deploy an SSH key, set up login into root using that key:
-#       - https://nixos.org/manual/nixos/stable/index.html#sec-ssh
-#       - Search for "deployment.keys" in NixOps manual.
 # TASK: Set `users.mutableUsers` to `false`:
 #       https://nixos.org/manual/nixos/stable/index.html#sec-user-management
 # TASK: Set up automatic upgrades:
