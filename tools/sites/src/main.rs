61a14b813276ed754fa481fb17ae7ad4ebe70024
Hanno Braun <hanno@braun-embedded.com>
2021-03-17 19:02:54 +0100

    Return `Result` from `main`

--- tools/sites/src/main.rs
+++ tools/sites/src/main.rs
@@ -1,6 +1,7 @@
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    Ok(())
 
     // TASK: Serve hanno.braun-odw.eu.
     // TASK: Redirect from braun-odw.eu to hanno.braun-odw.eu.
