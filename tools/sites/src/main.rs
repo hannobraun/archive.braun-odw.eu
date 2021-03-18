f8a8e67c04d7a808cfb02f15f7c5de93d7deab70
Hanno Braun <hanno@braun-embedded.com>
2021-03-18 19:37:05 +0100

    Refactor

--- tools/sites/src/main.rs
+++ tools/sites/src/main.rs
@@ -1,16 +1,13 @@
 use std::net::SocketAddr;
 
-use warp::Filter as _;
+use warp::{Filter, Rejection, Reply};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let hello = warp::path::end().map(|| format!("Hello, world!"));
 
-    let ipv4: SocketAddr = "127.0.0.1:8000".parse()?;
-    let ipv4 = warp::serve(hello).run(ipv4);
-
-    let ipv6: SocketAddr = "[::1]:8000".parse()?;
-    let ipv6 = warp::serve(hello).run(ipv6);
+    let ipv4 = serve(hello, "127.0.0.1:8000".parse()?);
+    let ipv6 = serve(hello, "[::1]:8000".parse()?);
 
     tokio::join!(ipv4, ipv6);
 
@@ -19,4 +16,12 @@ async fn main() -> anyhow::Result<()> {
     // TASK: Serve hanno.braun-odw.eu.
     // TASK: Redirect from braun-odw.eu to hanno.braun-odw.eu.
     // TASK: Redirect from www.braun-odw.eu to hanno.braun-odw.eu.
+}
+
+async fn serve<F>(filter: F, addr: SocketAddr)
+where
+    F: Filter<Error = Rejection> + Clone + Send + Sync + 'static,
+    F::Extract: Reply,
+{
+    warp::serve(filter).run(addr).await
 }
