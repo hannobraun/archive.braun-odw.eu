9458485bf2e2ace54ba0a61284452ad1309ca9ab
Hanno Braun <hanno@braun-embedded.com>
2021-03-20 18:49:52 +0100

    Update task

--- tools/packages/src/main.rs
+++ tools/packages/src/main.rs
@@ -1,5 +1,6 @@
 // TASK: Package `packages/sites` as Nix package and deploy to Reineke:
-//       https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
+//       - https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
+//       - https://nixos.wiki/wiki/Packaging/Tutorial
 
 fn main() {
     println!("Hello, world!");
