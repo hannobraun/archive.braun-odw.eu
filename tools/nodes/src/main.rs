9b998f0367fb817e7bf2e477cc4491b59fa3849c
Hanno Braun <hanno@braun-embedded.com>
2021-03-20 19:51:49 +0100

    Refactor

--- tools/nodes/src/main.rs
+++ tools/nodes/src/main.rs
@@ -3,9 +3,8 @@ use util::secrets::Secrets;
 fn main() -> anyhow::Result<()> {
     let secrets = Secrets::load()?;
 
-    let name = "nodes-test";
     util::docker::build_and_run(
-        name,
+        "nodes-test",
         "tools/nodes/docker/Dockerfile",
         &format!("SSH_KEY={}", secrets.nodes["reineke"].ssh_key),
     )?;
