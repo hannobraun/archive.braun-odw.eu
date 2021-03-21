1b74d31f4220b455dbf5fb3b09c540bd832e7811
Hanno Braun <hanno@braun-embedded.com>
2021-03-21 18:32:20 +0100

    Split task

---
+++ packages/sites/default.nix
@@ -0,0 +1,3 @@
+# TASK: Package `packages/sites` as Nix package:
+#       - https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
+#       - https://nixos.wiki/wiki/Packaging/Tutorial
--- tools/packages/src/main.rs
+++ tools/packages/src/main.rs
@@ -1,7 +1,3 @@
-// TASK: Package `packages/sites` as Nix package and deploy to Reineke:
-//       - https://nixos.org/manual/nixpkgs/stable/#compiling-rust-applications-with-cargo
-//       - https://nixos.wiki/wiki/Packaging/Tutorial
-
 fn main() -> anyhow::Result<()> {
     util::docker::build_and_run(
         "packages-test",
@@ -9,6 +5,8 @@ fn main() -> anyhow::Result<()> {
         &[],
         "packages/",
     )?;
+
+    // TASK: Deploy `sites`.
 
     Ok(())
 }
