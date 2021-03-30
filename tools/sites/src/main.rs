ad05d300320e9233b2f4d8826d315c6ae6a73a5f
Hanno Braun <hb@hannobraun.de>
2021-03-30 08:22:15 +0000

    Refactor

--- tools/sites/src/main.rs
+++ tools/sites/src/main.rs
@@ -5,11 +5,19 @@ use fs_extra::dir::CopyOptions;
 fn main() -> anyhow::Result<()> {
     let output_dir = Path::new("output");
 
-    if output_dir.exists() {
-        fs::remove_dir_all(output_dir)?;
-    }
-    fs::create_dir_all(output_dir)?;
+    prepare_output_dir(output_dir)?;
     fs_extra::copy_items(&["sites"], output_dir, &CopyOptions::new())?;
+
+    Ok(())
+}
+
+fn prepare_output_dir(path: impl AsRef<Path>) -> anyhow::Result<()> {
+    let path = path.as_ref();
+
+    if path.exists() {
+        fs::remove_dir_all(path)?;
+    }
+    fs::create_dir_all(path)?;
 
     Ok(())
 }
