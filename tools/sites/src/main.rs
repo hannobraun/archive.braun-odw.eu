b765dc1c168bdb9c04689aa5bf2749670a6e11af
Hanno Braun <hb@hannobraun.de>
2021-03-30 08:17:02 +0000

    Serve website from output directory

--- tools/sites/src/main.rs
+++ tools/sites/src/main.rs
@@ -1,4 +1,15 @@
+use std::{fs, path::Path};
+
+use fs_extra::dir::CopyOptions;
+
 fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let output_dir = Path::new("output");
+
+    if output_dir.exists() {
+        fs::remove_dir_all(output_dir)?;
+    }
+    fs::create_dir_all(output_dir)?;
+    fs_extra::copy_items(&["sites"], output_dir, &CopyOptions::new())?;
+
     Ok(())
 }
