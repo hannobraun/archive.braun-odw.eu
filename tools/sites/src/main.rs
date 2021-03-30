f3f5e6a6081ee6f8b8768b44d33e82f96c6fa19e
Hanno Braun <hb@hannobraun.de>
2021-03-30 08:54:01 +0000

    Serve sites, if `--serve` is passed

--- tools/sites/src/main.rs
+++ tools/sites/src/main.rs
@@ -10,12 +10,15 @@ use self::args::Args;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    println!("serve: {}", args.serve);
 
     let output_dir = Path::new("output");
 
     prepare_output_dir(output_dir)?;
     copy_sites(output_dir)?;
+
+    if args.serve {
+        serve_sites(output_dir).await?;
+    }
 
     Ok(())
 }
@@ -33,5 +36,14 @@ fn prepare_output_dir(path: impl AsRef<Path>) -> anyhow::Result<()> {
 
 fn copy_sites(output_dir: impl AsRef<Path>) -> anyhow::Result<()> {
     fs_extra::copy_items(&["sites"], output_dir, &CopyOptions::new())?;
+    Ok(())
+}
+
+async fn serve_sites(output_dir: impl AsRef<Path>) -> anyhow::Result<()> {
+    let sites_dir = output_dir.as_ref().join("sites");
+
+    let filter = warp::filters::fs::dir(sites_dir);
+    warp::serve(filter).run(([127, 0, 0, 1], 34480)).await;
+
     Ok(())
 }
