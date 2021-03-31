2ddcfa87f5dba4c8c2889e95565edda244e189d1
Hanno Braun <hb@hannobraun.de>
2021-03-31 10:02:09 +0000

    Add HTTP request logging

--- tools/sites/src/serve.rs
+++ tools/sites/src/serve.rs
@@ -1,11 +1,12 @@
 use std::path::Path;
 
-pub async fn serve_sites(output_dir: impl AsRef<Path>) -> anyhow::Result<()> {
-    // TASK: Add logging.
+use warp::Filter as _;
 
+pub async fn serve_sites(output_dir: impl AsRef<Path>) -> anyhow::Result<()> {
     let sites_dir = output_dir.as_ref().join("sites");
 
-    let filter = warp::filters::fs::dir(sites_dir);
+    let filter =
+        warp::filters::fs::dir(sites_dir).with(warp::filters::log::log(""));
     warp::serve(filter).run(([127, 0, 0, 1], 34480)).await;
 
     Ok(())
