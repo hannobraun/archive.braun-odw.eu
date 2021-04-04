cc665fc16d9538c7eae56646733ba91b56147bc5
Hanno Braun <hb@hannobraun.de>
2021-04-04 10:26:51 +0000

    Add task

--- tools/sites/src/serve.rs
+++ tools/sites/src/serve.rs
@@ -3,6 +3,8 @@ use std::path::PathBuf;
 use warp::Filter as _;
 
 pub async fn serve_sites(output_dir: impl Into<PathBuf>) -> anyhow::Result<()> {
+    // TASK: Serve a list of links to all available sites at `/`.
+
     let filter = warp::filters::fs::dir(output_dir.into())
         .with(warp::filters::log::log(""));
     warp::serve(filter).run(([127, 0, 0, 1], 34480)).await;
