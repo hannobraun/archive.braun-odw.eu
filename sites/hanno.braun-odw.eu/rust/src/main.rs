8b29d641752e054572b0c6b46fdd45449deb44c9
Hanno Braun <hanno@braun-odw.eu>
2021-05-31 18:19:02 +0000

    Call build function from site code

--- sites/hanno.braun-odw.eu/rust/src/main.rs
+++ sites/hanno.braun-odw.eu/rust/src/main.rs
@@ -1,5 +1,11 @@
+use ssg::build::build_once;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let source_dir = "sites";
+    let output_dir = "output";
+
+    build_once(source_dir, output_dir, None).await?;
+
     Ok(())
 }
