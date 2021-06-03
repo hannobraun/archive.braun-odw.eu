06bf6ed2af69a821a419facec841788416f55472
Hanno Braun <hanno@braun-odw.eu>
2021-06-03 09:02:46 +0000

    Enable logging in all site builders

--- sites/archive.braun-odw.eu/rust/src/main.rs
+++ sites/archive.braun-odw.eu/rust/src/main.rs
@@ -2,6 +2,7 @@ use ssg::{args::Args, build::build_once};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
     let args = Args::parse();
 
     build_once(&args.source, &args.target, None).await?;
