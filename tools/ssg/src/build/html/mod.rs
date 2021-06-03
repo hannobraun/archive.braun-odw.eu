83b8c7992fb1bfd68b51b64fc067787a8b5aa32c
Hanno Braun <hanno@braun-odw.eu>
2021-06-03 09:08:57 +0000

    Add logging

--- tools/ssg/src/build/html/mod.rs
+++ tools/ssg/src/build/html/mod.rs
@@ -8,6 +8,7 @@ use std::{
 };
 
 use tokio::{fs::File, io::AsyncWriteExt as _};
+use tracing::info;
 
 use self::model::Element;
 
@@ -17,6 +18,8 @@ pub async fn build(
 ) -> io::Result<()> {
     let output = output_dir.as_ref().join("index.html");
     let mut target = Vec::new();
+
+    info!("Building HTML document `{}`", output.display());
 
     writeln!(target, "<!DOCTYPE html>")?;
     html.write_to(&mut target)?;
