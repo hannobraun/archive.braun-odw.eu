f002a4407f0743009f2c86d24be4dc715090cb6e
Hanno Braun <hanno@braun-odw.eu>
2021-06-05 12:13:27 +0000

    Define `footer` element

--- tools/ssg/src/build/html/front_builder.rs
+++ tools/ssg/src/build/html/front_builder.rs
@@ -14,7 +14,7 @@ macro_rules! elements {
     };
 }
 
-elements!(a, address, base, div, h3, header, hr, li, p, span,);
+elements!(a, address, base, div, footer, h3, header, hr, li, p, span,);
 
 macro_rules! attributes {
     ($($name:ident,)*) => {
