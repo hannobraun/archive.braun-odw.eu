60658555f42915b8c81a21e0b14f5f75cdf1c360
Hanno Braun <hanno@braun-odw.eu>
2021-06-06 08:06:58 +0000

    Implement `Component` for components

--- tools/ssg-derive/src/lib.rs
+++ tools/ssg-derive/src/lib.rs
@@ -1,6 +1,16 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Component)]
-pub fn hello_macro_derive(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let output = quote! {
+        impl ssg::Component for #name {}
+    };
+
+    output.into()
 }
