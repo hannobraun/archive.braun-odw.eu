f829fe911f7e1d02039590ce72d4b6dc838f0e3d
Hanno Braun <hanno@braun-odw.eu>
2021-06-07 11:28:03 +0000

    Refactor

--- sites/hanno.braun-odw.eu/rust/src/side_projects.rs
+++ sites/hanno.braun-odw.eu/rust/src/side_projects.rs
@@ -1,5 +1,4 @@
 use ssg::{
-    html,
     html::{front_builder::*, model::Element, Content},
     Component,
 };
@@ -9,100 +8,92 @@ use crate::util;
 // TASK: Download side projects from external data files. TOML with a `content`
 //       key that contains Markdown would probably be best.
 pub fn side_projects() -> Element {
-    html! {
-        section {
-            { h2().with("Side Projects") }
-            {
-                p().with(
-                    "I always have an ongoing side project, and I try \
-                    to work on it every day. Here's a list of projects \
-                    I've completed recently.",
-                )
-            }
-            {
-                ol().with((
-                    side_project()
-                        .title("Fornjot: Spacer")
-                        .date("2021-05-19")
-                        .description((
-                            p().with((
-                                util::ext_link(
-                                    "https://github.com/hannobraun/fornjot",
-                                )
-                                .text("Fornjot"),
-                                " is an experimental programmatic CAD \
-                                system. I've been working on multiple \
-                                iterations of it over a few years, and \
-                                this is the first public release.",
-                            )),
-                            p().with((
-                                "The goal for this first release was to \
-                                build enough infrastructure to support ",
-                                util::ext_link(
-                                    "https://github.com/hannobraun/fornjot/tree/main/models/spacer",
-                                )
-                                .text("a simple spacer model"),
-                                ". I plan to extend it and create more \
-                                complex models going forward.",
-                            ))
-                        )),
-                    side_project()
-                        .title("braun-odw.eu")
-                        .date("2021-04-06")
-                        .description(
-                            p().with((
-                                "I revamped my website (the one you're \
-                                looking at right now) and put it on new \
-                                technical footing, using my own ",
-                                util::ext_link(
-                                    "https://github.com/hannobraun/braun-odw.eu",
-                                )
-                                .text("homegrown static site generator"),
-                                ". I also got a new domain and started \
-                                consolidating most of my IT infrastructure \
-                                (including email and Matrix homeserver) \
-                                under it.",
-                            ))
-                        ),
-                    side_project()
-                        .title("My Boss: Contacts")
-                        .date("2021-03-05")
-                        .description((
-                            p().with((
-                                util::ext_link(
-                                    "https://github.com/hannobraun/my-boss",
-                                )
-                                .text("My Boss"),
-                                " is a command-line application that tells me \
-                                what to do and when to do it. Kind of like an \
-                                ERP system, except that I'm the opposite of an \
-                                enterprise. This was the initial release that \
-                                included contact management functionality, \
-                                kind of like a CRM.",
-                            )),
-                            p().with(
-                                "I use it every day to remember to keep in \
-                                touch with my business contacts (and also some \
-                                personal ones). I plan to extend it in the \
-                                future to encompass more areas of my business, \
-                                like basic bookkeeping and possibly more."
-                            )
-                        )),
-                ))
-            }
-            {
-                p().with((
-                    "There's a lot more on my GitHub accounts (",
-                    util::ext_link("https://github.com/hannobraun")
-                        .text("personal"),
-                    " and ",
-                    util::ext_link("https://github.com/braun-embedded")
-                        .text("professional"),
-                    "), if you want to see more.",
-                ))
-            }
-        }
-    }
+    section().with((
+        h2().with("Side Projects"),
+        p().with(
+            "I always have an ongoing side project, and I try \
+            to work on it every day. Here's a list of projects \
+            I've completed recently.",
+        ),
+        ol().with((
+            side_project()
+                .title("Fornjot: Spacer")
+                .date("2021-05-19")
+                .description((
+                    p().with((
+                        util::ext_link(
+                            "https://github.com/hannobraun/fornjot",
+                        )
+                        .text("Fornjot"),
+                        " is an experimental programmatic CAD \
+                        system. I've been working on multiple \
+                        iterations of it over a few years, and \
+                        this is the first public release.",
+                    )),
+                    p().with((
+                        "The goal for this first release was to \
+                        build enough infrastructure to support ",
+                        util::ext_link(
+                            "https://github.com/hannobraun/fornjot/tree/main/models/spacer",
+                        )
+                        .text("a simple spacer model"),
+                        ". I plan to extend it and create more \
+                        complex models going forward.",
+                    ))
+                )),
+            side_project()
+                .title("braun-odw.eu")
+                .date("2021-04-06")
+                .description(
+                    p().with((
+                        "I revamped my website (the one you're \
+                        looking at right now) and put it on new \
+                        technical footing, using my own ",
+                        util::ext_link(
+                            "https://github.com/hannobraun/braun-odw.eu",
+                        )
+                        .text("homegrown static site generator"),
+                        ". I also got a new domain and started \
+                        consolidating most of my IT infrastructure \
+                        (including email and Matrix homeserver) \
+                        under it.",
+                    ))
+                ),
+            side_project()
+                .title("My Boss: Contacts")
+                .date("2021-03-05")
+                .description((
+                    p().with((
+                        util::ext_link(
+                            "https://github.com/hannobraun/my-boss",
+                        )
+                        .text("My Boss"),
+                        " is a command-line application that tells me \
+                        what to do and when to do it. Kind of like an \
+                        ERP system, except that I'm the opposite of an \
+                        enterprise. This was the initial release that \
+                        included contact management functionality, \
+                        kind of like a CRM.",
+                    )),
+                    p().with(
+                        "I use it every day to remember to keep in \
+                        touch with my business contacts (and also some \
+                        personal ones). I plan to extend it in the \
+                        future to encompass more areas of my business, \
+                        like basic bookkeeping and possibly more."
+                    )
+                )),
+        )),
+        p().with((
+            "There's a lot more on my GitHub accounts (",
+            util::ext_link("https://github.com/hannobraun")
+                .text("personal"),
+            " and ",
+            util::ext_link("https://github.com/braun-embedded")
+                .text("professional"),
+            "), if you want to see more.",
+        )),
+    ))
 }
 
 #[derive(Component)]
