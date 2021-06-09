2b7a99de1a2f27d27a0f842bf6227b2950c802e6
Hanno Braun <hanno@braun-odw.eu>
2021-06-09 17:52:49 +0000

    Refactor

--- sites/hanno.braun-odw.eu/rust/src/html/side_projects.rs
+++ sites/hanno.braun-odw.eu/rust/src/html/side_projects.rs
@@ -7,26 +7,33 @@ use crate::data;
 
 use super::util;
 
-pub fn side_projects(side_projects: data::SideProjects) -> Element {
-    section((
-        h2("Side Projects"),
-        p("I always have an ongoing side project, and I try \
-            to work on it every day. Here's a list of projects \
-            I've completed recently."),
-        ol(Content::from_iter(
-            side_projects
-                .into_iter()
-                .map(|project| side_project(project)),
-        )),
-        p((
-            "There's a lot more on my GitHub accounts (",
-            util::ext_link("https://github.com/hannobraun").text("personal"),
-            " and ",
-            util::ext_link("https://github.com/braun-embedded")
-                .text("professional"),
-            "), if you want to see more.",
-        )),
-    ))
+#[derive(Component)]
+pub struct SideProjects(data::SideProjects);
+
+impl From<SideProjects> for Element {
+    fn from(side_projects: SideProjects) -> Self {
+        section((
+            h2("Side Projects"),
+            p("I always have an ongoing side project, and I try \
+                to work on it every day. Here's a list of projects \
+                I've completed recently."),
+            ol(Content::from_iter(
+                side_projects
+                    .0
+                    .into_iter()
+                    .map(|project| side_project(project)),
+            )),
+            p((
+                "There's a lot more on my GitHub accounts (",
+                util::ext_link("https://github.com/hannobraun")
+                    .text("personal"),
+                " and ",
+                util::ext_link("https://github.com/braun-embedded")
+                    .text("professional"),
+                "), if you want to see more.",
+            )),
+        ))
+    }
 }
 
 #[derive(Component)]
