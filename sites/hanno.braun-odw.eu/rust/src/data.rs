804e53a5c904348a0b67cd3c1d8eb2da61894494
Hanno Braun <hanno@braun-odw.eu>
2021-06-09 17:57:12 +0000

    Refactor

--- sites/hanno.braun-odw.eu/rust/src/data.rs
+++ sites/hanno.braun-odw.eu/rust/src/data.rs
@@ -4,6 +4,62 @@ use ssg::markdown::Markdown;
 
 pub struct SideProjects(pub Vec<SideProject>);
 
+impl SideProjects {
+    // TASK: Load side projects from external data files.
+    pub fn load() -> Self {
+        Self(vec![
+            SideProject {
+                title: "Fornjot: Spacer",
+                date: "2021-05-19",
+                description: Markdown("\
+                    [Fornjot] is an experimental programmatic CAD system. I've \
+                    been working on multiple iterations of it over a few \
+                    years, and this is the first public release.\n\n\
+
+                    The goal for this first release was to build enough \
+                    infrastructure to support [a simple spacer model]. I plan \
+                    to extend it and create more complex models going \
+                    forward.\n\n\
+
+                    [Fornjot]: https://github.com/hannobraun/fornjot\n\
+                    [a simple spacer model]: https://github.com/hannobraun/fornjot/tree/main/models/spacer
+                "),
+            },
+            SideProject {
+                title: "braun-odw.eu",
+                date: "2021-04-06",
+                description: Markdown("\
+                    I revamped my website (the one you're looking at right \
+                    now) and put it on new technical footing, using my own \
+                    [homegrown static site generator]. I also got a new domain \
+                    and started consolidating most of my IT infrastructure \
+                    (including email and Matrix homeserver) under it.\n\n\
+
+                    [homegrown static site generator]: https://github.com/hannobraun/braun-odw.eu
+                "),
+            },
+            SideProject {
+                title: "My Boss: Contacts",
+                date: "2021-03-05",
+                description: Markdown("\
+                    [My Boss] is a command-line application that tells me what \
+                    to do and when to do it. Kind of like an ERP system, \
+                    except that I'm the opposite of an enterprise. This was \
+                    the initial release that included contact management \
+                    functionality, kind of like a CRM.\n\n\
+
+                    I use it every day to remember to keep in touch with my \
+                    business contacts (and also some personal ones). I plan to \
+                    extend it in the future to encompass more areas of my \
+                    business, like basic bookkeeping and possibly more.\n\n\
+
+                    [My Boss]: https://github.com/hannobraun/my-boss
+                "),
+            }
+        ])
+    }
+}
+
 impl IntoIterator for SideProjects {
     type Item = SideProject;
 
