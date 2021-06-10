de53488f32cd92dc51e96c34ffa42243d3487130
Hanno Braun <hanno@braun-odw.eu>
2021-06-10 12:41:30 +0000

    Refactor

--- sites/hanno.braun-odw.eu/rust/src/html/ongoing_work.rs
+++ sites/hanno.braun-odw.eu/rust/src/html/ongoing_work.rs
@@ -1,6 +1,6 @@
 use ssg::{
     html,
-    html::{front_builder::*, model::Element},
+    html::{front_builder::*, model::Element, Content},
     Component,
 };
 
@@ -13,52 +13,46 @@ pub struct OngoingWork;
 
 impl From<OngoingWork> for Element {
     fn from(_: OngoingWork) -> Self {
+        let items = vec![
+            ongoing_work_item(data::OngoingWorkItem {
+                title: "Braun Embedded",
+                link: "https://braun-embedded.com/",
+                description: html! {
+                    p {
+                        "I provide software \
+                        development and consulting \
+                        services, specifically \
+                        relating to Rust firmware on \
+                        ARM Cortex-M microcontrollers. \
+                        If you want to write your next \
+                        firmware project in Rust and \
+                        could use some help with the \
+                        low-level stuff, "
+                        {
+                            util::email()
+                                .text("let me know")
+                        }
+                        "!"
+                    }
+                },
+            }),
+            ongoing_work_item(data::OngoingWorkItem {
+                title: "Made by Hanno",
+                link: "https://madeby.hannobraun.de/",
+                description: html! {
+                    p {
+                        "I have a small workshop where \
+                        I make semi-interesting stuff, \
+                        mainly 3D-printed."
+                    }
+                },
+            }),
+        ];
+
         html! {
             section {
                 h2 { "Ongoing Work" }
-                ul {
-                    {
-                        ongoing_work_item(
-                            data::OngoingWorkItem {
-                                title: "Braun Embedded",
-                                link: "https://braun-embedded.com/",
-                                description: html! {
-                                    p {
-                                        "I provide software \
-                                        development and consulting \
-                                        services, specifically \
-                                        relating to Rust firmware on \
-                                        ARM Cortex-M microcontrollers. \
-                                        If you want to write your next \
-                                        firmware project in Rust and \
-                                        could use some help with the \
-                                        low-level stuff, "
-                                        {
-                                            util::email()
-                                                .text("let me know")
-                                        }
-                                        "!"
-                                    }
-                                },
-                            }
-                        )
-                    }
-                    {
-                        ongoing_work_item(
-                            data::OngoingWorkItem {
-                                title: "Made by Hanno",
-                                link: "https://madeby.hannobraun.de/",
-                                description: html! {
-                                    p {
-                                        "I have a small workshop where \
-                                        I make semi-interesting stuff, \
-                                        mainly 3D-printed."
-                                    }
-                                },
-                            }
-                        )
-                    }
-                }
+                ul { { Content::from_iter(items) } }
             }
         }
     }
