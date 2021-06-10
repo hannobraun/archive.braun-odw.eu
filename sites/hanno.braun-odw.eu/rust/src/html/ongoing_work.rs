9fcb0cf4cbab51c3cf34fac9720e7c6eca401634
Hanno Braun <hanno@braun-odw.eu>
2021-06-10 12:26:18 +0000

    Rename variable

--- sites/hanno.braun-odw.eu/rust/src/html/ongoing_work.rs
+++ sites/hanno.braun-odw.eu/rust/src/html/ongoing_work.rs
@@ -5,7 +5,7 @@ use super::util;
 pub fn ongoing_work_item(
     title: &'static str,
     link: &'static str,
-    content: Element,
+    description: Element,
 ) -> Element {
-    li((header((h3(title), util::ext_link(link))), content))
+    li((header((h3(title), util::ext_link(link))), description))
 }
